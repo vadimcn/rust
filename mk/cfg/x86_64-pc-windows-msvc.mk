fb77e3c68cc073be5b397cba21dbb0c6a95cc35e
Alex Crichton <alex@alexcrichton.com>
2015-05-11 13:59:51 -0700

    configure: Start adding MSVC support
    This commit starts to add MSVC support to the ./configure script to enable the
    build system to detect and build an MSVC target with the cl.exe compiler and
    toolchain. The primary change here is a large sanity check when an MSVC target
    is requested (and currently only `x86_64-pc-windows-msvc` is recognized).
    
    When building an MSVC target, the configure script either requires the
    `--msvc-root` argument or for `cl.exe` to be in `PATH`. It also requires that if
    in the path `cl.exe` is the 64-bit version of the compiler.
    
    Once detected the configure script will run the `vcvarsall.bat` script provided
    by Visual Studio to learn about the `INCLUDE` and `LIB` variables needed by the
    `cl.exe` compiler to run (the default include/lib paths for the
    compiler/linker). These variables are then reexported when running `make` to
    ensure that our own compiles are running the same toolchain.
    
    The purpose of this detection and environment variable scraping is to avoid
    requiring the build itself to be run inside of a `cmd.exe` shell but rather
    allow it to run in the currently expected MinGW/MSYS shell.

--- mk/cfg/x86_64-pc-windows-msvc.mk
+++ mk/cfg/x86_64-pc-windows-msvc.mk
@@ -1,9 +1,9 @@
 # x86_64-pc-windows-msvc configuration
-CC_x86_64-pc-windows-msvc=cl
-LINK_x86_64-pc-windows-msvc=link
-CXX_x86_64-pc-windows-msvc=cl
-CPP_x86_64-pc-windows-msvc=cl
-AR_x86_64-pc-windows-msvc=llvm-ar
+CC_x86_64-pc-windows-msvc=$(CFG_CL) -nologo
+LINK_x86_64-pc-windows-msvc=$(CFG_LINK) -nologo
+CXX_x86_64-pc-windows-msvc=$(CFG_CL) -nologo
+CPP_x86_64-pc-windows-msvc=$(CFG_CL) -nologo
+AR_x86_64-pc-windows-msvc=$(CFG_LIB) -nologo
 CFG_LIB_NAME_x86_64-pc-windows-msvc=$(1).dll
 CFG_STATIC_LIB_NAME_x86_64-pc-windows-msvc=$(1).lib
 CFG_LIB_GLOB_x86_64-pc-windows-msvc=$(1)-*.dll
@@ -21,4 +21,8 @@ CFG_UNIXY_x86_64-pc-windows-msvc :=
 CFG_LDPATH_x86_64-pc-windows-msvc :=
 CFG_RUN_x86_64-pc-windows-msvc=$(2)
 CFG_RUN_TARG_x86_64-pc-windows-msvc=$(call CFG_RUN_x86_64-pc-windows-msvc,,$(2))
-CFG_GNU_TRIPLE_x86_64-pc-windows-msvc := x86_64-pc-windows-msvc
+CFG_GNU_TRIPLE_x86_64-pc-windows-msvc := x86_64-pc-win32
+
+export INCLUDE := $(CFG_MSVC_INCLUDE)
+export LIB := $(CFG_MSVC_LIB)
+NATIVE_TOOL_DEPS_core_T_x86_64-pc-windows-msvc += llvm-ar.exe
